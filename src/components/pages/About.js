c6d4190fe2c8788adfaacd63f55592ef4a6d3b8a
Ben Grinthal <bgrinthal@gmail.com>
2022-07-24 22:13:49 -0400

    about me punctuation fixed

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,16 +14,16 @@ export default function About() {
         <div className='col-sm-9 mt-5'>
           <h2 className='title typewriter mb-4'>Hello World!  I am Ben Grinthal, Full Stack Dev.</h2>
           <p>
-            I am a Rutgers University graduate with a bachelors degree in mechanical engineering and a
-            certificate in aerospace engineering. Over the last 10 years I have gained experience in different
-            engineering fields from manufacturing to quality control to design, learning the ins and outs of
+            I am a Rutgers University graduate with a bachelor's degree in mechanical engineering and a
+            certificate in aerospace engineering. Over the last 10 years, I have gained experience in different
+            engineering fields from manufacturing to quality control to design, learning the mechanics of
             the engineering profession. Through this time, I discovered a growing interest in coding, teaching myself some and
-            eventually enrolling in a Rutgers Coding Bootcamp course.  During this intensive bootcamp, I learned bany different 
+            eventually enrolling in a Rutgers Coding Bootcamp course.  During this intensive bootcamp, I learned many different 
             languages and technologies shown on my resume and demonstrated in my projects.
             <br></br>
             <br></br>
             I am a team player, dependable, and enjoy learning new skills and technologies.  My goal is to take my years of 
-            professional experience and problem soling skills aquired from my previous engineering positions, along with the 
+            professional experience and problem-solving skills acquired from my previous engineering positions, along with the 
             knowledge I am gaining from this course, to kickstart my career in coding.  
             </p>
         </div>
